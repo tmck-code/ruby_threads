6cd7fdc1a4a5f74fa9cfe9ca2a47b142e67b1b79
Tom McKeesick <tmck01@gmail.com>
2016-09-21 18:06:19 +1000

    Classifying each character to use different types together more easily

--- unicode_char_info/unicode_25xx_test.rb
+++ unicode_char_info/unicode_25xx_test.rb
@@ -8,14 +8,27 @@ class UnicodeTest
   START = "\u2500".ord.freeze
   FIN   = "\u257F".ord.freeze
   GROUPS = {
-    ch_straight: [
+    straight_h: [
       "\u2500", "\u2501", "\u2508", "\u2509", "\u254c", "\u254d",
       "\u2550", "\u2574", "\u2576", "\u257c", "\u257e"
+    ],
+    straight_v: [
+      "\u2502", "\u2503", "\u2506", "\u2507", "\u250a", "\u250b",
+      "\u254e", "\u254f", "\u2551", "\u2579", "\u257b", "\u257d",
+      "\u257f"
     ],
     t_section_r: [
       "\u251c", "\u251d", "\u251e", "\u251f", "\u2520", "\u2520",
       "\u2521", "\u2522", "\u2523", "\u255e", "\u255f", "\u2560",
       "\u2560"
+    ],
+    t_section_d: [
+      "\u252d", "\u252e", "\u252f", "\u2530", "\u2531", "\u2532",
+      "\u2533", "\u2564", "\u2565", "\u2566"
+    ],
+    elbow_u_r: [
+      "\u2514", "\u2515", "\u2516", "\u2517", "\u2558", "\u2559",
+      "\u255a", "\u2570"
     ]
   }.freeze
 
@@ -55,8 +68,11 @@ class UnicodeTest
 
   def print_groups
     GROUPS.each do |key, group|
-      puts "> #{key}"
-      group.each { |ch| printf('%2s', ch) }
+      puts "> #{key}\n".underline
+      group.each_with_index do |ch, i|
+        printf('%4s', ch)
+        print "\n\n" if (i + 1) % @width == 0
+      end
       puts "\n\n"
     end
   end
